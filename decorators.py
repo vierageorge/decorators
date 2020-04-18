ace8af3f7a1f559d9635575e4b1b848022ed3d21
vierageorge <joviera@gmail.com>
2020-04-18 11:47:38 -0500

    Adds arguments and return to decorators

--- decorators.py
+++ decorators.py
@@ -1,10 +1,19 @@
+import functools
 from datetime import datetime
 
 def during_start_of_minute(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         actual_sec = datetime.now().second
         if 0 <= actual_sec < 30:
-            func()
+            func(*args, **kwargs)
         else:
             print(actual_sec)
+    return wrapper
+
+def do_twice(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from datetime import datetime
-from decorators import during_start_of_minute
+from decorators import during_start_of_minute, do_twice
 
-@during_start_of_minute
+@do_twice
 def say_whee():
     print(f"Wheeee! {datetime.now().second}")
 
+@do_twice
+def greet(name):
+    print(f"Hi {name}! How are yo doing?")
+
 if __name__ == '__main__':
-    say_whee()
+    g = greet("Jorge")
+    s = say_whee()
